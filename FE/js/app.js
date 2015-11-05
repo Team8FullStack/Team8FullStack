78ff6293c7bd6a2781304c561b8ad6eb4b1baa24
Grady Rogers <gradyrogers@Gradys-MacBook-Pro.local>
2015-11-05 16:05:59 -0500

    obj literal on app.js

---
+++ FE/js/app.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+  app.init();
+});
+
+var app = {
+  init: function() {
+    app.events();
+    app.events();
+    app.styling();
+  },
+
+  events: function() {
+
+  },
+
+  styling: function() {
+
+  }
+};
