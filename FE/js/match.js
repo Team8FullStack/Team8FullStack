a30445193d7125886b0e1c3992665b264da948c0
Grady Rogers <gradyrogers@Gradys-MacBook-Pro.local>
2015-11-07 12:51:37 -0500

    getting matches

--- FE/js/match.js
+++ FE/js/match.js
@@ -1,18 +1,25 @@
-activeUser = {
 
-};
 
 var get = {
-  checkExistingUsers: function() {
+  closestMatch: function() {
     $.ajax({
       url: '/get-users',  //insert json reference
       method: 'GET',
       success: function(data) {
         Mdata = JSON.parse(data);
-        console.log(Mdata);
+
+        // active user should be global var set from login
+        activeUser = Mdata[0];
+
+        _.each(Mdata, function(currVal, idx, arr) {
+          if (currVal.username != activeUser.username && currVal.gender != activeUser.gender) {
+            matchArray.push(currVal);
+
+          }
+        });
       },
       fail: function() {
-        console.log("Username or password does not exist. Please create a new account");
+        console.log("Error finding match");
       }
     });
   },
