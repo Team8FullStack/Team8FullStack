6216f76cb0cdaed3eefb737081baf05a1d9dd8d9
Grady Rogers <gradyrogers@Gradys-MacBook-Pro.local>
2015-11-07 16:17:02 -0500

    get closest match

--- FE/js/match.js
+++ FE/js/match.js
@@ -1,20 +1,23 @@
-
+//////////////////////////////////////////// READ ME ////////////////////////////////
+// Getting matches is dependent on the login function
+// login must define activeUser located on app.js
+// get.allMatches(); must run first
+// get.closestMatch(); returns one variation of opposite sex same stereotype
+/////////////////////////////////////////////////////////////////////////////////////
 
 var get = {
-  closestMatch: function() {
+  allMatches: function() {
     $.ajax({
       url: '/get-users',  //insert json reference
       method: 'GET',
       success: function(data) {
         Mdata = JSON.parse(data);
 
-        // active user should be global var set from login
-        activeUser = Mdata[0];
-
         _.each(Mdata, function(currVal, idx, arr) {
+          // checks for opposite sex and not the activeUser user username
           if (currVal.username != activeUser.username && currVal.gender != activeUser.gender) {
+            // populates the matchArray with all possible matches for any filter
             matchArray.push(currVal);
-
           }
         });
       },
@@ -23,4 +26,32 @@ var get = {
       }
     });
   },
+
+  closestMatch: function() {
+    var userType = activeUser.stereotype.typeName;
+    // this is where the match is picked from for this 'closest' filter
+    sameTypeArray = [];
+
+    _.each(matchArray, function(currVal, idx, arr) {
+      // checks for the same stereotype
+      if (currVal.stereotype.typeName === userType) {
+        sameTypeArray.push(currVal);
+      }
+    });
+
+     // idxNum is set to a random whole number between 0 and the length of the array - 1
+    idxNum = Math.floor(Math.random() * (sameTypeArray.length - 1)) + 0;
+    match = sameTypeArray[idxNum];
+
+    console.log(match);
+
+  },
+
+  oppositeMatch: function() {
+
+  },
+
+  randomMatch: function() {
+
+  }
 };
